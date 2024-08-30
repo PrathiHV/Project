25136c508e2f90d55c30afe04fb2fdf972eec955
PrathiHV <prathihv@gmail.com>
2024-08-30 18:34:43 +0530

    Update Alerts.java

--- src/test/java/DemoTools/Alerts.java
+++ src/test/java/DemoTools/Alerts.java
@@ -2,32 +2,6 @@
 
 package DemoTools;
 
-/*import org.openqa.selenium.By;
-
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebElement;
-import org.testng.annotations.Test;
-
-public class Alerts  extends BaseClass {
-	@Test
-    public void element() throws InterruptedException 
-	 {
-		Thread.sleep(3000);
-		 JavascriptExecutor js=(JavascriptExecutor) driver;
-		 WebElement title = driver.findElement(By.xpath("//h5[text()='Alerts, Frame & Windows']"));
-		 Thread.sleep(2000);
-		 js.executeScript("arguments[0].scrollIntoView();",title );
-		 Thread.sleep(2000);
-	   	 title.click();
-	    Thread.sleep(2000);
-	    driver.findElement(By.xpath("//span[text()='Alerts']")).click();
-	    Thread.sleep(2000);
-	   
-         
-}
-}*/
-
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
